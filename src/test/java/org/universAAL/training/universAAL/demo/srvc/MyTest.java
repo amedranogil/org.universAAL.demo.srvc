f48f49603ec3956b796a22a53e69a5b90b02dda1
Alejandro M. Medrano Gil <amedrano@lst.tfo.upm.es>
2017-11-13 15:45:08 +0100

    Template created for service bus example.

---
+++ src/test/java/org/universAAL/training/universAAL/demo/srvc/MyTest.java
@@ -0,0 +1,10 @@
+package org.universAAL.training.universAAL.demo.srvc;
+
+import org.universAAL.middleware.bus.junit.OntTestCase;
+
+public class MyTest extends OntTestCase {
+
+    public void test() {
+    	generateOntFiles4MyProy();
+    }
+}
---
+++ src/test/java/org/universAAL/training/universAAL/demo/srvc/UnitTest.java
@@ -0,0 +1,44 @@
+package org.universAAL.training.universAAL.demo.srvc;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * UnitTest Class, a collection of unitary tests of different methods in the module.
+ *
+ * For each set of test create a <code>public void *test()</code> method, then
+ * check the correct status by adding assertions.
+ *
+ * @see TestCase
+ * @see <a href=http://junit.org/junit4/> JUnit framework</a>
+ *
+ */
+public class UnitTest extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public UnitTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( UnitTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
